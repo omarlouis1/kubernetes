00feda4e169baa354c4aeceb1f8df541dadd8ea6
Seynabou26 <seynaboubadji26@gmail.com>
2025-10-02 21:37:15 +0000

    first-commit

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import Classe from './components/Classe.jsx'
-
-createRoot(document.getElementById('root')).render(
-    
-    <Classe />
-)
