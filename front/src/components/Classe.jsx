c1378524bac93bae09c2d173197cdd53b9dfb910
Seynabou26 <seynaboubadji26@gmail.com>
2025-10-14 22:03:27 +0000

    reussi

--- front/src/components/Classe.jsx
+++ front/src/components/Classe.jsx
@@ -7,7 +7,8 @@ import EditerSmartphone from "./EditerSmartphone.jsx";
 // -------------------------------
 // CORRECTION : Utilisez la variable correctement
 //const API_BASE = "http://localhost:5000/api/smartphones";
-const API_BASE = `${process.env.REACT_APP_API_URL || 'http://localhost:5000/api'}/smartphones`;
+//const API_BASE = `${process.env.REACT_APP_API_URL || 'http://localhost:5000/api'}/smartphones`;
+const API_BASE = `${import.meta.env.VITE_API_URL || 'http://localhost:5000/api'}/smartphones`;
 // -------------------------------
 
 function Classe() {
